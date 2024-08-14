78edfa272c21c551ad19055d11194263bdb6b3b0
mehendisil2004 <115357567+mehendisil2004@users.noreply.github.com>
2024-08-15 02:12:34 +0530

    Update

--- OOP/q16.cpp
+++ OOP/q16.cpp
@@ -4,34 +4,16 @@ using namespace std;
 class Point3D
 {
 private:
-    double x;
-    double y;
-    double z;
+    double x, y, z;
 
 public:
-    Point3D();
-    Point3D(double x, double y, double z);
-    static double distance(Point3D, Point3D);
+    Point3D(double x=0, double y=0, double z=0) : x(x), y(y), z(z) {}
+
+    double distance(Point3D p1, Point3D p2)
+    {
+        return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2) + pow(p1.z - p2.z, 2));
+    }
 };
-
-Point3D::Point3D()
-{
-    this->x = 0.0;
-    this->y = 0.0;
-    this->z = 0.0;
-}
-
-Point3D::Point3D(double x, double y, double z)
-{
-    this->x = x;
-    this->y = y;
-    this->z = z;
-}
-
-double Point3D::distance(Point3D p1, Point3D p2)
-{
-    return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2) + pow(p1.z - p2.z, 2));
-}
 
 int main() {
     double x1, y1, z1, x2, y2, z2;
@@ -43,7 +25,7 @@ int main() {
     Point3D p1(x1, y1, z1);
     Point3D p2(x2, y2, z2);
 
-    cout << "Distance = " << Point3D::distance(p1, p2) << endl;
+    cout << "Distance = " << p2.distance(p1, p2) << endl;
 
     return 0;
 }
