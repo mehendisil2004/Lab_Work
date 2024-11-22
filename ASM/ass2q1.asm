152a8af6ea6537de5237352e88ac9076d0456a90
Mehendi Sil <mehendisil4@gmail.com>
2024-11-23 01:47:13 +0530

    Update ass2q1.asm

--- ASM/ass2q1.asm
+++ ASM/ass2q1.asm
@@ -1,6 +1,9 @@
 .model small
-.code 
+.stack 100h
+.data
+.code
 main proc
+
 mov ax, 3000h
 mov ds, ax
 mov si, 0030h
@@ -19,6 +22,9 @@ l1:
 inc si
 loop looping
 mov [di], dl
+
 int 03h
+mov ah, 4ch
+int 21h
 main endp
-end
+end main
