a2edb320bdd0e8e333a9e83157c54077bf1cfd30
Mehendi Sil <mehendisil4@gmail.com>
2024-12-23 01:34:57 +0530

    Update set1q9.c

--- DSA/set1q9.c
+++ DSA/set1q9.c
@@ -5,14 +5,25 @@ int getIndex(int row, int col, int numCols) {
 }
 
 int main() {
-    int rows = 3, cols = 4;
-    int array[3][4] = {
-        {1, 2, 3, 4},
-        {5, 6, 7, 8},
-        {9, 10, 11, 12}
-    };
+    int rows, cols;
 
+    // Ask for the number of rows and columns
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter the number of columns: ");
+    scanf("%d", &cols);
+
+    int array[rows][cols];
     int oneDArray[rows * cols];
+
+    // Input the 2D array from the user
+    printf("Enter the elements of the 2D array:\n");
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            printf("Enter element at position (%d, %d): ", i + 1, j + 1);
+            scanf("%d", &array[i][j]);
+        }
+    }
 
     // Convert 2D array to 1D array
     for (int i = 0; i < rows; ++i) {
