5cced7e31805df93b055dbc915b5f0b866275ba7
Mehendi Sil <mehendisil4@gmail.com>
2025-01-22 00:16:16 +0530

    Update q1.java

--- OOS/q1.java
+++ OOS/q1.java
@@ -13,7 +13,7 @@ class Room {
 }
 
 class RoomDemo {
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         Room r= new Room(7, 10, 12);
 
         System.out.println("Vol of room: " +r.vol());
